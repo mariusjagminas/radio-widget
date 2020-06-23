a53b56d0645395f9b90419d907b36519bcf899b7
mariusjagminas <mariusjagminas.it@gmail.com>
2020-06-23 09:25:30 +0200

    Add test for Stations component

---
+++ src/components/Stations/Stations.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Stations from "./Stations";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
+
+describe("<Stations/>", () => {
+  it("should render 8 stations", () => {
+    render(
+      <Provider store={store}>
+        <Stations />
+      </Provider>
+    );
+
+    expect(screen.getAllByRole("listitem").length).toBe(8);
+  });
+});
